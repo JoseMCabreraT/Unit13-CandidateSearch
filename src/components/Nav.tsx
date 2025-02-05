78f1f013792f78324c0af2306ae8d1f715b4c3a2
Jose Cabrera <josemcabrerat@gmail.com>
2025-02-05 06:51:57 -0700

    A added the token to the .env file. Also, I created the interface Candidates.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,57 @@
+import { Link, useLocation } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
+  const currentPage = useLocation().pathname;
+
+
   return (
-    <div>Nav</div>
+    <div>
+<h1>
+        <Link to='/' id='logo'>
+          
+        </Link>
+      </h1>
+      <ul className='nav nav-tabs'>
+        <li className='nav-item'>
+          <h2>
+            <Link
+              to='/'
+              className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+            >
+              TEST 1
+            </Link>
+          </h2>
+        </li>
+        <li className='nav-item'>
+          <h2>
+            <Link
+              to='/WatchList'
+              className={
+                currentPage === '/WatchList' ? 'nav-link active' : 'nav-link'
+              }
+            >
+              TES 2
+            </Link>
+          </h2>
+        </li>
+        <li className='nav-item'>
+          <h2>
+            <Link
+              to='/SeenIt'
+              className={
+                currentPage === '/SeenIt' ? 'nav-link active' : 'nav-link'
+              }
+            >
+              TEST 3
+            </Link>
+          </h2>
+        </li>
+      </ul>
+
+
+
+    </div>
   )
 };
 
--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+
+export default interface Candidates {
+    readonly FullName: string | null;
+    readonly Username: string | null;
+    readonly Location: string | null;
+    readonly Avatar: string | null;
+    readonly Email: string | null;
+    readonly html_url: string | null;
+    readonly Company: string | null;
+
+}//interface Candidates
