ebef49036ad114a98d7d5b5dd5ba5a95934a819a
Jose Cabrera <josemcabrerat@gmail.com>
2025-02-07 05:49:30 -0700

    I added more updates on SavedCandidates.tsx.

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,7 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import type Candidates from '../utils/Candidate.interface';
+
 const SavedCandidates = () => {
+  const clearLocalStorage = () => {
+    localStorage.removeItem('candidates');
+  };
+
   return (
     <>
       <h1>Potential Candidates</h1>
+      <button onClick={clearLocalStorage}>Clear Candidates</button>
     </>
   );
 };
